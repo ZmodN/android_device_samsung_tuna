98a454f496c5d9ad56d148f583b4fce484e709d2
Noeljunior <liamgliam@gmail.com>
2015-11-23 12:17:30 +0000

    Revert "add aosp bootanimation"
    This reverts commit 9a7abfbbb005ada4fd7f0a88cf4f8c0abefdbadf.

--- device.mk
+++ device.mk
@@ -217,9 +217,6 @@ PRODUCT_PACKAGES += \
 	fibmap.f2fs \
 	f2fstat
 
-#AOSP bootanimation
-PRODUCT_BOOTANIMATION := $(DEVICE_FOLDER)/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip
-
 # DCC
 PRODUCT_PACKAGES += \
 	dumpdcc
